b053eb7a1daad6c91d6855bd226629e0e3960867
EkaliVan <ekalivan4@gmail.com>
2026-08-12 22:31:58 +0630

    feat: implement insert function for trie

--- autocomplete.c
+++ autocomplete.c
@@ -1,10 +1,11 @@
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
 
-
 #define ALPHABET_SIZE 26
 
+// Node Structure
 typedef struct TrieNode
 {
     bool isEnd;
@@ -12,13 +13,50 @@ typedef struct TrieNode
 } TrieNode;
 
 TrieNode *create_node (void);
+void insert(TrieNode *node, const char *word);
 
+// Main function
 int main (void)
 {
-    TrieNode node1 = *create_node();
-    printf("%p\n", node1);
+//  Root node 
+    TrieNode *root = create_node();
+
+    // Insert test word
+    insert(root, "ant");
+
+    //  Manual Testing
+    // 'a' 
+    if (root->child['a' - 'a'] != NULL)
+    {
+        printf("Found 'a'\n");
+
+        // 'n'  
+        if (root->child['a' - 'a']->child['n' - 'a'] != NULL)
+        {
+            printf("Found 'n'\n");
+
+            // 't'  
+            if (root->child['a' - 'a']->child['n' - 'a']->child['t' - 'a'] != NULL)
+            {
+                printf("Found 't'\n");
+
+                // Check isEnd == True 
+                if (root->child['a' - 'a']->child['n' - 'a']->child['t' - 'a']->isEnd)
+                {
+                    printf("SUCCESS: 'ant' is correctly inserted!\n");
+                }
+            }
+        }
+    }
+    else
+    {
+        printf("FAILED: Insertion failed!\n");
+    }
+
+    return 0;
 }
 
+// Functions
 TrieNode *create_node (void)
 {
     TrieNode *node = malloc(sizeof(TrieNode));
@@ -34,4 +72,23 @@ TrieNode *create_node (void)
         node->child[i] = NULL;
     }
     return node;
+}
+
+void insert(TrieNode *root, const char *word)
+{
+    TrieNode *curr = root;
+
+    for (int i = 0; word[i] != '\0'; i++)
+    {
+        int index = toupper(word[i]) - 'A';
+
+        if (curr->child[index] == NULL)
+        {
+            curr->child[index] = create_node();
+        }
+
+        curr = curr->child[index];
+    }
+
+    curr->isEnd = true;
 }
